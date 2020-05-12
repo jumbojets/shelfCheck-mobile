6a3efb30c4a8f21ac9646bdfb64fb1612d4f2a38
James Taylor <jamestaylor3@protonmail.com>
2020-05-12 00:56:54 -0400

    cleaned up home screen

--- constants/Colors.js
+++ constants/Colors.js
@@ -1,4 +1,4 @@
-const tintColor = '#2f95dc';
+const tintColor = '#7e84f3';
 
 export default {
   tintColor,
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -8,9 +8,7 @@ import { MonoText } from '../components/StyledText';
 
 export default function HomeScreen() {
   return (
-    
-    <View style={styles.container}>
-      <View style={styles.container} contentContainerStyle={styles.contentContainer}>
+      <View style={styles.container}>
       <ImageBackground source={require('../assets/images/background.png')} style={{width: '100%', height: '100%'}}>
         <View style={styles.welcomeContainer}>
           <Image
@@ -30,7 +28,7 @@ export default function HomeScreen() {
 
             <Text style={styles.questionText}>What would you{"\n"}like to find?</Text>
 
-            <View style={{width:"100%", height: "30%", backgroundColor:"white", flexDirection:"row", justifyContent: "space-evenly"}}>
+            <View style={{width:"100%", height: "32%", backgroundColor:"white", flexDirection: "row", justifyContent: "space-evenly"}}>
 
               <TouchableOpacity style={{height: "100%", width: "40%", borderRadius: "90", backgroundColor: "#7256f3"}} />
 
@@ -38,7 +36,7 @@ export default function HomeScreen() {
 
             </View>
 
-            <View style={{width:"100%", height: "30%", backgroundColor:"white", flexDirection: "row", justifyContent: "space-evenly"}}>
+            <View style={{width:"100%", height: "32%", backgroundColor:"white", flexDirection: "row", justifyContent: "space-evenly"}}>
 
               <TouchableOpacity style={{height: "100%", width: "40%", borderRadius: "90", backgroundColor: "#7b85f4", alignItems: "center", flexDirection: "column", justifyContent: "space-evenly"}}>
 
@@ -74,54 +72,17 @@ export default function HomeScreen() {
       </View>
 
       
-    </View>
-  );
-}
-
-HomeScreen.navigationOptions = {
-  header: null,
-};
-
-function DevelopmentModeNotice() {
-  if (__DEV__) {
-    const learnMoreButton = (
-      <Text onPress={handleLearnMorePress} style={styles.helpLinkText}>
-        Learn more
-      </Text>
-    );
-
-    return (
-      <Text style={styles.developmentModeText}>
-        Development mode is enabled: your app will be slower but you can use useful development
-        tools. {learnMoreButton}
-      </Text>
-    );
-  } else {
-    return (
-      <Text style={styles.developmentModeText}>
-        You are not in development mode: your app will run at full speed.
-      </Text>
-    );
-  }
-}
-
-function handleLearnMorePress() {
-  WebBrowser.openBrowserAsync('https://docs.expo.io/versions/latest/workflow/development-mode/');
-}
-
-function handleHelpPress() {
-  WebBrowser.openBrowserAsync(
-    'https://docs.expo.io/versions/latest/get-started/create-a-new-app/#making-your-first-change'
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     backgroundColor: '#fff',
+    height: "100%",
   },
   itemCategoryButtonsContainer: {
     backgroundColor: '#fff',
-    width: "85%",
+    width: "90%",
     height: "80%",
     borderRadius: 30,
     justifyContent: "space-evenly"
@@ -145,16 +106,6 @@ const styles = StyleSheet.create({
     fontSize: 30,
     left: "5%"
   },
-  developmentModeText: {
-    marginBottom: 20,
-    color: 'rgba(0,0,0,0.4)',
-    fontSize: 14,
-    lineHeight: 19,
-    textAlign: 'center',
-  },
-  contentContainer: {
-    paddingTop: 30,
-  },
   welcomeContainer: {
     alignItems: 'center',
     marginTop: 10,
@@ -166,65 +117,5 @@ const styles = StyleSheet.create({
     resizeMode: 'contain',
     marginTop: 3,
     marginLeft: -10,
-  },
-  getStartedContainer: {
-    alignItems: 'center',
-    marginHorizontal: 50,
-  },
-  homeScreenFilename: {
-    marginVertical: 7,
-  },
-  codeHighlightText: {
-    color: 'rgba(96,100,109, 0.8)',
-  },
-  codeHighlightContainer: {
-    backgroundColor: 'rgba(0,0,0,0.05)',
-    borderRadius: 3,
-    paddingHorizontal: 4,
-  },
-  getStartedText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    lineHeight: 24,
-    textAlign: 'center',
-  },
-  tabBarInfoContainer: {
-    position: 'absolute',
-    bottom: 0,
-    left: 0,
-    right: 0,
-    ...Platform.select({
-      ios: {
-        shadowColor: 'black',
-        shadowOffset: { width: 0, height: -3 },
-        shadowOpacity: 0.1,
-        shadowRadius: 3,
-      },
-      android: {
-        elevation: 20,
-      },
-    }),
-    alignItems: 'center',
-    backgroundColor: '#fbfbfb',
-    paddingVertical: 20,
-  },
-  tabBarInfoText: {
-    fontSize: 17,
-    color: 'rgba(96,100,109, 1)',
-    textAlign: 'center',
-  },
-  navigationFilename: {
-    marginTop: 5,
-  },
-  helpContainer: {
-    marginTop: 15,
-    alignItems: 'center',
-  },
-  helpLink: {
-    paddingVertical: 15,
-  },
-  helpLinkText: {
-    fontSize: 14,
-    color: '#2e78b7',
   },
 });
